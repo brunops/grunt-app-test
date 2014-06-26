870e8c381528856f01b81189cd22e57a7005a962
Bruno Sanches <brunopsanches@gmail.com>
2014-06-26 01:13:19 -0700

    Add jade task to grunt and fix browserify path

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,23 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     browserify: {
-      'public/app.js': ['/client/app.js']
+      'public/app.js': ['client/app.js']
+    },
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          'public/index.html': ['client/index.jade']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-jade');
 };
 
